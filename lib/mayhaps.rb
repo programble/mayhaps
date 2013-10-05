124917e423005123344c178bd9b8e02e1a388549
Curtis McEnroe <programble@gmail.com>
2013-10-05 14:10:21 -0400

    Initial commit

---
+++ lib/mayhaps.rb
@@ -0,0 +1,41 @@
+require 'singleton'
+
+module Mayhaps
+  VERSION = '0.1.0'
+
+  class Nothing
+    include Singleton
+
+    def method_missing(*args)
+      nil
+    end
+  end
+
+  class Chain
+    attr_reader :end
+
+    def initialize(obj)
+      @end = obj
+    end
+
+    def method_missing(*args, &block)
+      @end ? Chain.new(@end.public_send(*args, &block)) : self
+    end
+  end
+end
+
+class Object
+  def maybe
+    self
+  end
+
+  def maybe_chain
+    Mayhaps::Chain.new(self)
+  end
+end
+
+class NilClass
+  def maybe
+    Mayhaps::Nothing.instance
+  end
+end
